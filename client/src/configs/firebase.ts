f74cd38cf12c2db1cf33aec37fb161d3cab72354
atikx <atikshgupta6373@gmail.com>
2025-11-21 16:28:20 +0530

    initial commit

--- client/src/configs/firebase.ts
+++ client/src/configs/firebase.ts
@@ -1,17 +1,15 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app'
 import { getAnalytics } from 'firebase/analytics'
 import { getAuth, GoogleAuthProvider } from 'firebase/auth'
 
-// add your credential here
 const firebaseConfig = {
-  apiKey: 'AIzaSyAM6ioYKby_qMJ5D53QJVPFD2ofkv3c9cI',
-  authDomain: 'zens-caven.firebaseapp.com',
-  projectId: 'zens-caven',
-  storageBucket: 'zens-caven.firebasestorage.app',
-  messagingSenderId: '448067558310',
-  appId: '1:448067558310:web:e6eb8d825edc62df164d67',
-  measurementId: 'G-C2V8HTFNZG',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 }
 
 // Initialize Firebase
